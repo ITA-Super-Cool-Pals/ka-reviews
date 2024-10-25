aa9bbef8d9ba6e8314768541cab8c6350c547805
Josef-TL <j.tonnesenlucas@gmail.com>
2024-10-25 08:47:16 +0200

    started api routes

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import db_service
+from flask import Flask, jsonify
 
 db_service.init()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+# Get all reviews
+@app.route('/reviews', methods=['GET'])
+def get_reviews():
+    return jsonify(db_service.read_all()), 200
+
+if __name__ == '__main__':
+    db_service.init()  # Ensure the database is initialized before running
+    app.run(host='0.0.0.0', port=5000)
